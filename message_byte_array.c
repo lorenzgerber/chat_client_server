d37a27411ecd5aac864fcb2b5ebb530585d2732a
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-09-20 23:22:41 +0200

    function bodies for message_byte_array

--- message_byte_array.c
+++ message_byte_array.c
@@ -7,8 +7,43 @@
 #include "message_byte_array.h"
 
 
+
+
+
+
+int message_byte_array_add_uint8(message_byte_array *self, uint8_t data){
+
+	return 0;
+}
+
+
+int message_byte_array_add_uint16(message_byte_array *self, uint16_t data){
+
+	return 0;
+}
+
+
+int message_byte_array_add_uint32(message_byte_array *self, uint32_t data){
+
+	return 0;
+}
+
+
+int message_byte_array_add_chars(message_byte_array *self, char* data, uint32_t legnth){
+
+	return 0;
+}
+
+
 message_byte_array* create_message_byte_array(uint32_t length){
-	message_byte_array array = calloc(length, sizeof(uint8_t));
+	message_byte_array *array = calloc(length, sizeof(uint8_t));
+	array->add_uint8 = message_byte_array_add_uint8;
+	array->add_uint16 = message_byte_array_add_uint16;
+	array->add_uint32 = message_byte_array_add_uint32;
+	array->add_chars = message_byte_array_add_chars;
+
+	array->current_position = 0;
+
 	return array;
 
 }
--- message_byte_array.h
+++ message_byte_array.h
@@ -7,13 +7,29 @@
 
 #ifndef MESSAGE_BYTE_ARRAY_H_
 #define MESSAGE_BYTE_ARRAY_H_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include "pdu_templates.h"
 
 typedef struct message_byte_array {
 	uint8_t *array;
 	uint32_t current_position;
+	int (*add_uint8)(struct message_byte_array *self, uint8_t);
+	int (*add_uint16)(struct message_byte_array *self, uint16_t);
+	int (*add_uint32)(struct message_byte_array *self, uint32_t);
+	int (*add_chars)(struct message_byte_array *self, char*, uint32_t);
 
 } message_byte_array;
+
+
+int message_byte_array_add_uint8(message_byte_array *self, uint8_t data);
+int message_byte_array_add_uint16(message_byte_array *self, uint16_t data);
+int message_byte_array_add_uint32(message_byte_array *self, uint32_t data);
+int message_byte_array_add_chars(message_byte_array *self, char* data, uint32_t legnth);
+
 
 message_byte_array* create_message_byte_array(uint32_t length);
 
