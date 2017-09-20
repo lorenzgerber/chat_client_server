b194dfed8323172bd79b5508aa6cf2e43f0bda84
pajasson <1982konig@gmail.com>
2017-09-20 18:41:19 +0200

    create string of reg

--- pdu_parser.c
+++ pdu_parser.c
@@ -3,6 +3,19 @@
 
 
 char* pdu_reg_create_message(pdu_REG *self){
-	return "hello bullshit\n";
+
+    char type[1];
+    char port[4];
+    char nameLen[255];
+    sprintf(type, "%d", self->type);
+    sprintf(nameLen, "%d", self->server_name_length);
+    sprintf(port, "%d", self->tcp_port);
+
+    char* mess = malloc(sizeof(char)*(strlen(type)+strlen(nameLen)+strlen(port)+strlen(self->server_name)));
+    strcat(mess, type);
+    strcat(mess, nameLen);
+    strcat(mess, port);
+    strcat(mess, self->server_name);
+	return mess;
 }
 
