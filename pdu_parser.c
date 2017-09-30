7f6101760cc14f28ab61ae8cd5e142876e3ad13e
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-09-30 14:44:23 +0200

    new files for parser / deserializer

---
+++ pdu_parser.c
@@ -0,0 +1,12 @@
+/*
+ * pdu_parser.c
+ *
+ *  Created on: Sep 30, 2017
+ *      Author: lgerber
+ */
+
+#include "pdu_parser.h"
+
+int test(void){
+	return 0;
+}
---
+++ pdu_parser.h
@@ -0,0 +1,17 @@
+/*
+ * pdu_parser.h
+ *
+ *  Created on: Sep 30, 2017
+ *      Author: lgerber
+ */
+
+#ifndef PDU_PARSER_H_
+#define PDU_PARSER_H_
+
+#include <string.h>
+#include "pdu_templates.h"
+
+int test(void);
+
+
+#endif /* PDU_PARSER_H_ */
