f072a6ad727587cad8350cac13fc2ba75c3717f1
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-10-03 20:06:09 +0200

    new test dir with main for socket testing

---
+++ src/test/test_socket/test_socket.c
@@ -0,0 +1,16 @@
+/*
+ * test_socket.c
+ *
+ *  Created on: Oct 3, 2017
+ *      Author: lgerber
+ */
+#include <stdio.h>
+
+
+int main(int argc, char*argv[]){
+
+	printf("socket testing\n");
+
+	return 0;
+}
+
