dcd3150f13973f86c7a4dfcb949d8fe6862135ba
pajasson <1982konig@gmail.com>
2017-10-14 21:33:07 +0200

    successful join and rec perticipants

--- src/execs/client/main.c
+++ src/execs/client/main.c
@@ -9,21 +9,18 @@
 #include <socket_templates.h>
 #include <socket_creator.h>
 #include <linked_list.h>
-
-void get_list_to_user(pdu* slist, list* servers);
-void print_welcome(void);
-list* request_chat_servers();
-
 typedef struct chat_server{
     char name[255];
     char address[20];
     uint16_t port;
 }chat_server;
-
+void get_list_to_user(pdu* slist, list* servers);
+void print_welcome(void);
+list* request_chat_servers();
+int chat_loop(chat_server *server);
 int main (int argc, char*argv[]){
 
-
-
+    int joined;
     char *input;
     int bufsize = 255;
     __ssize_t characters;
@@ -32,9 +29,9 @@ int main (int argc, char*argv[]){
         perror("Unable to allocate buffer\n");
         EXIT_FAILURE;
     }
-
+    list* servers = list_empty();
     while(true){
-        list* servers = list_empty();
+
         print_welcome();
         memset(input, 0, (size_t) bufsize);
         characters = getline(&input, (size_t *) &bufsize, stdin);
@@ -47,24 +44,61 @@ int main (int argc, char*argv[]){
                 list_free(servers);
             }
             servers = request_chat_servers();
-            list_free(servers);
             continue;
         }else if(strcmp(input,"exit\n") == 0) {
             list_free(servers);
             break;
         }else if(strncmp(input,"join ",5) == 0){
             printf("\njoining %s\n", input+5);
+            list_position p = list_first(servers);
+            do{
+                if(p!=list_first(servers)){
+                    p=list_next(p);
+                }
+                chat_server* cs;
+                cs = (chat_server *) list_inspect(p);
+                printf("\n%s\n",cs->name);
+                if(strncmp(input+5, cs->name, strlen(cs->name))==0){
+                    printf("\n**%s**\n",cs->address);
+                    joined = chat_loop(cs);
+                    printf("\nsuccess\n");
+                }
+            } while(!list_is_end(servers, p));
             list_free(servers);
-            continue;
+            break;
         }else{
-            printf("unkown command\n");
-            list_free(servers);
+            printf("unknown command\n");
             continue;
         }
     }
 
     free(input);
 
+}
+
+int chat_loop(chat_server *server) {
+
+    io_handler* chat_server_com = create_tcp_client_communicator(server->address, server->port);
+    chat_server_com->connect(chat_server_com, 5);
+    pdu *join = create_join(8);
+    join->add_identity(join, "identity");
+    chat_server_com->send_pdu(chat_server_com, join);
+    free_join(join);
+
+    chat_server_com->status = 0;
+    pdu* participants = NULL;
+    // looping until an error happens or we get data
+    while(chat_server_com->status == STATUS_RECEIVE_EMPTY || chat_server_com->status == 0){
+        participants = parse_header(chat_server_com);
+    }
+    if(chat_server_com->status != STATUS_RECEIVE_OK){
+        printf("\nsomething wrong with receive in Client\n");
+        free_tcp_client_communicator(chat_server_com);
+    }else{
+        free_tcp_client_communicator(chat_server_com);
+    }
+    print_participants(participants);
+    return 0;
 }
 
 list* request_chat_servers() {
@@ -76,7 +110,7 @@ list* request_chat_servers() {
     name_server_com->connect(name_server_com, 5);
     pdu *getlist = create_getlist();
     name_server_com->send_pdu(name_server_com, getlist);
-    getlist->free_pdu(getlist);
+    free_getlist(getlist);
 
     name_server_com->status = 0;
     pdu* slist = NULL;
@@ -97,10 +131,25 @@ list* request_chat_servers() {
 
 void get_list_to_user(pdu* slist, list* servers){
     for(int i = 0; i< slist->number_servers;i++){
+        char address1[3] = {0};
+        char address2[3] = {0};;
+        char address3[3] = {0};;
+        char address4[3] = {0};;
 
         chat_server* server = malloc(sizeof(chat_server));
         strcpy(server->name, slist->current_servers[i]->name);
-        strcpy(server->address, (char *) slist->current_servers[i]->address);
+        sprintf(address1, "%d", slist->current_servers[i]->address[0]);
+        sprintf(address2, "%d", slist->current_servers[i]->address[1]);
+        sprintf(address3, "%d", slist->current_servers[i]->address[2]);
+        sprintf(address4, "%d", slist->current_servers[i]->address[3]);
+        strcpy(server->address, address1);
+        strcat(server->address, ".");
+        strcat(server->address, address2);
+        strcat(server->address, ".");
+        strcat(server->address, address3);
+        strcat(server->address, ".");
+        strcat(server->address, address4);
+
         server->port = slist->current_servers[i]->port;
 
         list_insert(list_first(servers),server);
