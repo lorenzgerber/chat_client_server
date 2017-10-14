d3a4209007b1ad0d7d12af7ac15613318ed4bead
pajasson <1982konig@gmail.com>
2017-10-14 04:18:49 +0200

    started on chatclient

--- src/execs/client/main.c
+++ src/execs/client/main.c
@@ -8,35 +8,94 @@
 #include <stdio.h>
 #include <socket_templates.h>
 #include <socket_creator.h>
+#include <linked_list.h>
+
+void print_slist_servers(pdu* slist);
+void print_welcome(void);
+
+void request_chat_servers(io_handler *com);
 
 int main (int argc, char*argv[]){
 
-	printf("I'm the super chat client\n");
+	//list* servers = list_empty();
 
-	io_handler* client = create_tcp_client_communicator("itchy.cs.umu.se", 1337);
-	client->connect(client, 5);
-	client->status = 0;
+    char *input;
+    int bufsize = 255;
+    __ssize_t characters;
+    int command = 1;
+    input = (char *)malloc(bufsize * sizeof(char));
+    if( input == NULL) {
+        perror("Unable to allocate buffer\n");
+        EXIT_FAILURE;
+    }
 
-	pdu *getlist = create_getlist();
+    io_handler* name_server_com = create_tcp_client_communicator("itchy.cs.umu.se", 1337);
+    name_server_com->connect(name_server_com, 5);
+    name_server_com->status = 0;
 
-	client->send_pdu(client, getlist);
-	getlist->free_pdu(getlist);
+    while(command){
 
-	pdu* slist;
-	// looping until an error happens or we get data
-	while(client->status == STATUS_RECEIVE_EMPTY || client->status == 0){
-		slist = parse_header(client);
-	}
-	if(client->status != STATUS_RECEIVE_OK){
-		printf("something wrong with receive in Client\n");
-	}else{
-		client->status = 0;
+        print_welcome();
+        memset(input, 0, (size_t) bufsize);
+        characters = getline(&input, (size_t *) &bufsize, stdin);
 
-		slist->print(slist);
-		slist->free_pdu(slist);
-	}
+        if(characters > bufsize){
+            printf("input too long\n");
+            continue;
+        }else if(strcmp(input,"/servers\n") == 0){
+            request_chat_servers(name_server_com);
+            name_server_com->status = 0;
+            continue;
+        }else if(strcmp(input,"/exit\n") == 0){
+            break;
+        }
+        printf("%zu characters were read.\n",characters);
+        printf("You typed: '%s'\n",input);
+        command = 0;
+    }
 
-	free_tcp_client_communicator(client);
+    free(input);
+	free_tcp_client_communicator(name_server_com);
+}
 
+void request_chat_servers(io_handler *com) {
 
+    pdu *getlist = create_getlist();
+
+    com->send_pdu(com, getlist);
+    getlist->free_pdu(getlist);
+
+    pdu* slist = NULL;
+    // looping until an error happens or we get data
+    while(com->status == STATUS_RECEIVE_EMPTY || com->status == 0){
+        slist = parse_header(com);
+    }
+    if(com->status != STATUS_RECEIVE_OK){
+
+        printf("\nsomething wrong with receive in Client\n");
+        return;
+    }else{
+        print_slist_servers(slist);
+        return;
+    }
+}
+
+void print_slist_servers(pdu* slist){
+    for(int i = 0; i< slist->number_servers;i++){
+        printf("\nName: %s, address: %d.%d.%d.%d, port: %d\n",
+               slist->current_servers[i]->name,
+               slist->current_servers[i]->address[0],
+               slist->current_servers[i]->address[1],
+               slist->current_servers[i]->address[2],
+               slist->current_servers[i]->address[3],
+               slist->current_servers[i]->port);
+    }
+    slist->free_pdu(slist);
+}
+
+void print_welcome(void){
+    printf("\nWelcome to the super chat client!\n");
+    printf("'/help' displays a list of commands\n");
+    printf("Type the name or address of an active chat server to join\n");
+    printf(">");
 }
